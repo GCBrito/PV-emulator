b1723e650a9a0e622c6e9a36373320b7a79c4686
Gabriel Ceolin de Brito <gabriel.ceolindebrito@student-cs.fr>
2025-12-28 04:49:21 +0100

    Update compute_5_parameters.m

--- Single-diode model/Auxiliary Algorithms/MATLAB/compute_5_parameters.m
+++ Single-diode model/Auxiliary Algorithms/MATLAB/compute_5_parameters.m
@@ -1,39 +1,42 @@
 clc; clear; close all; format long
+
 %% Module Parameters
 
 ns      = 60;       % number of series cells
 np      = 1;        % number of parallel branches
 
-Vmp_mod_ref  = 27.5;     % voltage at maximum power point (V)
-Imp_mod_ref  = 6.60;     % current at maximum power point (A)
-Voc_mod_ref  = 34.2;     % open-circuit voltage (V)
-Isc_mod_ref  = 7.19;     % short-circuit current (A)
+Vmp_mod_ref  = 30.1;     % voltage at maximum power point (V)
+Imp_mod_ref  = 8.30;     % current at maximum power point (A)
+Voc_mod_ref  = 37.2;     % open-circuit voltage (V)
+Isc_mod_ref  = 8.87;     % short-circuit current (A)
 
 Tref = 25 + 273.15; % Reference temperature (K)
+Sref = 1000; % Reference irradiance (W/m²)
+
+alpha   = 0.00065;  % temperature coefficient of Isc (%/K)
+beta = -0.0037; % temperature coefficient of Voc (%/K)
 
 %% Operating Conditions
 
-Vmp_cell_ref = Vmp_mod_ref/ns;
-Imp_cell_ref = Imp_mod_ref/np;
-Voc_cell_ref = Voc_mod_ref/ns;
-Isc_cell_ref = Isc_mod_ref/np;
+T = 44.5 + 273.15; % Current temperature (K)
+S = 765; % Current irradiance (W/m²)
 
 %% Physical Constants
 
 q = 1.60217662e-19; % Elementary charge (C)
 k = 1.38064852e-23; % Boltzmann constant (J/K)
-
 E_G0 = 1.166;            % Band gap energy at 0K (eV)
 k1   = 4.73e-4;          % Coefficient k1 (eV/K)
 k2   = 636;              % Coefficient k2 (K)
 
 %% Parameter Estimation via fsolve
 
-x0 = [Isc_cell_ref; 1e-9; 3; 0.01; 2];
+x0 = [Isc_mod_ref; 1e-9; ns; 0.05; 500];
 opts = optimoptions('fsolve', ...
     'Display', 'off', 'TolFun', 1e-10, 'TolX', 1e-10, 'MaxIter', 1000, 'MaxFunctionEvaluations', 2000);
-fun = @(x) residuals_2_20(x, Voc_cell_ref, Isc_cell_ref, Vmp_cell_ref, Imp_cell_ref, q, k, Tref);
+fun = @(x) residuals_2_20(x, Voc_mod_ref, Isc_mod_ref, Vmp_mod_ref, Imp_mod_ref, q, k, Tref);
 [xsol, ~, exitflag] = fsolve(fun, x0, opts);
+
 if exitflag <= 0
     warning('fsolve did not converge (exitflag = %d)', exitflag);
 end
@@ -54,17 +57,13 @@ fprintf('Rp      = %.6f Ohms\n', Rp);
 
 function F = residuals_2_20(x, Voc, Isc, Vmp, Imp, q, k, Tref)
     Iph = x(1); Is0 = x(2); A = x(3); Rs = x(4); Rp = x(5);
-
     C   = q/(A*k*Tref);
-
     cap = 700;
     a_sc = min(C*Rs*Isc, cap);
     a_oc = min(C*Voc, cap);
     a_mp = min(C*(Rs*Imp+Vmp), cap);
-    a_eq5 = min(C*Is0, cap); 
-
+    a_eq5 = min(C*Isc, cap); 
     F = zeros(5,1);
-
     F(1) = Isc - ( Iph - Is0*(exp(a_sc)-1) - (Rs*Isc)/Rp );
     F(2) = 0   - ( Iph - Is0*(exp(a_oc)-1) - (Voc)/Rp );
     F(3) = Imp - ( Iph - Is0*(exp(a_mp)-1) - (Rs*Imp+Vmp)/Rp );
@@ -73,3 +72,39 @@ function F = residuals_2_20(x, Voc, Isc, Vmp, Imp, q, k, Tref)
     F(5) = Iph - 2*Vmp/Rp + Is0 - Is0 * term_coeff * exp(a_mp);
 end
 
+function I = solve_I_V_2_11(V, Iph_ref, Is0_ref, A, Rs, Rp, ...
+    q, k, S, Sref, alpha, T, Tref, E_G0, k1, k2, ns) 
+    
+    % 1. Photocurrent (Iph) 
+    Iph = Iph_ref * (S / Sref) * (1 + alpha * (T - Tref));
+    
+    % 2. Calculation of Saturation Current (Is)
+    
+    % 2a. Calculation of band gap energy in eV at temperature T: Eg(T)
+    Eg_T_eV = E_G0 - (k1 * T^2) / (T + k2);
+    
+    % 2b. Calculation of the temperature difference term
+    temp_diff = (1 / Tref) - (1 / T);
+    
+    % 2c. Calculation of the exponent 
+    exponent_term = (ns*q / (A * k)) * Eg_T_eV * temp_diff; 
+    
+    % 2d. Final calculation of Is(T)
+    Is = Is0_ref * (T / Tref)^3 * exp(exponent_term);
+    
+    % 3. Iterative Solver (Newton-Raphson)
+    I = Iph; % Initial guess
+    for it = 1:30
+        V_diode = V + I * Rs;
+        arg_exp = min(q * V_diode / (A * k * T), 700); 
+        expo = exp(arg_exp);
+        
+        f = Iph - Is * (expo - 1) - V_diode / Rp - I;
+        df = -Is * expo * (q * Rs / (A * k * T)) - Rs / Rp - 1;
+        
+        dI = -f / df;
+        I = I + dI;
+        
+        if abs(dI) < 1e-6, break;end
+    end
+end
